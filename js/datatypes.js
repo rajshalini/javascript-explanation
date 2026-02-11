ef36e84c66b8373d0cc7939e9fb71fc9e0a0e6cb
shalini <146663295+rajshalini@users.noreply.github.com>
2026-02-11 09:33:25 +0000

    container config done

---
+++ js/datatypes.js
@@ -0,0 +1,17 @@
+let name="shalini"
+let age =18;
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value(is a object)
+// undefined => 
+// symbol => unique
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof name);
+console.log(typeof isloggedIn);
---
+++ js/variables.js
@@ -0,0 +1,6 @@
+// console.log("shalini")
+let accountId=101
+var accountState ="Jaipur"
+const accountName="Shalini"
+accountCity="Delhi"
+console.table([accountId, accountName, accountState])
